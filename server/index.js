b7c23510c9ffe408e79b596980a9d25c445ecdee
Yug <03yugagarwal@gmail.com>
2024-10-28 18:23:27 +0530

    protected all routes except login for teacher and student

--- server/index.js
+++ server/index.js
@@ -14,16 +14,19 @@ app.get('/',(req,res)=>{
     res.send('Hello World')
 })
 
-const AssignmentRoute = require('./routes/AssignmentRoute')
-app.use('/student',verifyToken, AssignmentRoute);
-const UserRoute = require('./routes/UserRoute')
-app.use('/user',UserRoute)
-const CourseRoute = require('./routes/CourseRoute')
-app.use('/course',verifyToken, CourseRoute)
-const TeacherRoute = require('./routes/TeacherRoute')
-app.use('/teacher',TeacherRoute)
 const ValidateRoute = require('./routes/ValidateRoute')
 app.use('/validate',ValidateRoute)
+const TeacherRoute = require('./routes/TeacherRoute')
+app.use('/teacher',TeacherRoute)
+const UserRoute = require('./routes/UserRoute')
+app.use('/user',UserRoute)
+
+app.use(verifyToken)
+
+const CourseRoute = require('./routes/CourseRoute')
+app.use('/course', CourseRoute)
+const AssignmentRoute = require('./routes/AssignmentRoute')
+app.use('/student', AssignmentRoute);
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log(`Server started on http://localhost:${process.env.PORT || 3000}`);
